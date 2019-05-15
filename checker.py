4a35b06be9379d06cab72aef500e2eebc1b9a215
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2019-05-15 23:05:56 +0400

    Added threading, added locks on global counter for progress

--- checker.py
+++ checker.py
@@ -2,9 +2,14 @@
 
 import requests
 import argparse
+import threading
+from threading import Thread
+
+global_url_counter = 0
+misconfigured_urls = list()
 
 def setup_parser():
-	parser = argparse.ArgumentParser(description='Git Misconfiguration Checker')
+	parser = argparse.ArgumentParser(description='A MultiThreaded Git Misconfiguration Checker')
 	parser.add_argument('-f', '--file', type=str, help="File Location", required=True)
 	parser.add_argument('-t', '--threads', type=int, help="Threads to Spawn", required=False)
 	args = parser.parse_args()
@@ -34,35 +39,51 @@ def validate_url(url):
 	return url
 
 def print_progress_bar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filled_length = int(length * iteration // total)
-    bar = fill * filled_length + '-' * (length - filled_length)
-    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+	percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+	filled_length = int(length * iteration // total)
+	bar = fill * filled_length + '-' * (length - filled_length)
+	print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
 
-    if iteration == total: 
-        print()
+	if iteration == total: 
+		print()
+
+def send_request(url, result, counter, total_urls, lock):
+	global global_url_counter
+
+	filtered_url = validate_url(url.rstrip('\n'))
+	new_url = filtered_url.rstrip('\n') + ".git/"
+
+	try:
+		check_request = requests.get(new_url, timeout=5)
+		if check_request.status_code == 200:
+			misconfigured_urls.append(new_url)
+	except requests.exceptions.Timeout:
+		pass
+
+	lock.acquire()
+
+	global_url_counter += 1
+	print_progress_bar(global_url_counter, total_urls, prefix = 'Progress:', suffix = 'Complete', length = 50)
+	
+	lock.release()
+
 
 def main():
 	file_location, num_threads = setup_parser()
 	urls = open(file_location, "r").readlines()
 
-	misconfigured_urls = list()
+	print_progress_bar(0, len(urls), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
-	printProgressBar(0, len(urls), prefix = 'Progress:', suffix = 'Complete', length = 50)
+	threads = []
 
 	for counter, url in enumerate(urls):
-		filtered_url = validate_url(url.rstrip('\n'))
-		new_url = filtered_url.rstrip('\n') + ".git/"
+		process = Thread(target=send_request, args=[url, counter, counter, len(urls), threading.Lock()])
+		process.start()
+		threads.append(process)
 
-		try:
-			check_request = requests.get(new_url, timeout=5)
-			if check_request.status_code == 200:
-				misconfigured_urls.append(new_url)
-		except requests.exceptions.Timeout:
-			pass
-
-		print_progress_bar(counter + 1, len(urls), prefix = 'Progress:', suffix = 'Complete', length = 50)
-
+	for process in threads:
+		process.join()
+		
 	for misconfigured_url in misconfigured_urls:
 		print("[+] " + misconfigured_url)
 
